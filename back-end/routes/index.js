4095293ccd92250c35c885d9f33c7dccb0d48abe
Marissa Monivis <mmonivis@gmail.com>
2017-07-06 10:04:55 -0400

    Updates to Express BackEnd

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getStudents', function(req, res, next) {
+  res.json({
+  	msg: "Success!"
+  })
 });
 
 module.exports = router;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+    	super(props);
+    	this.state = {
+    		theClass: []
+    	}
+    }
+
+    componentDidMount() {
+        $.getJSON('http://localhost:3000/getStudents', (studentsFromApi)=>{
+            console.log(studentsFromApi)
+        });
+        this.setState({
+            theClass: [1,2,3,4]
+        })
+    }
+
+	render() {
+
+		var theClassArray = [];
+		this.state.theClass.map((student,index)=>{
+            theClassArray.push(<li key={index}>Student</li>)
+		});
+
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+                <p>
+                    {theClassArray}
+                </p>
+			</div>
+		);
+	}
 }
 
 export default App;
